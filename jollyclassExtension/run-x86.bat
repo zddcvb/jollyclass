fa32331db151327117dff08770d3156e0366e9cb
zddcvb <zddcvb@163.com>
2018-12-30 16:31:50 +0800

    five commit

---
+++ jollyclassExtension/run-x86.bat
@@ -0,0 +1,3 @@
+java -jar adt.jar -package -tsa none -storetype pkcs12 -keystore key.p12 -storepass 123456 -target ane JollyClassAne.ane extension.xml -swc JollyClassAne.swc -platform Android-x86 JollyClassAneExtension.jar library.swf
+
+pause
